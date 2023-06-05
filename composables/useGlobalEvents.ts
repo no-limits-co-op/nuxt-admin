5eb7e7f926a8f1fbda271555cc32ed94b6c1281b
huzhuangzhuang <zhuangzhuang@dao42.com>
2023-06-05 22:09:24 +0800

    perf: extract unocss

---
+++ composables/useGlobalEvents.ts
@@ -0,0 +1,21 @@
+export default function useGlobalEvents() {
+  const route = useRoute()
+  const { locale, t } = useI18n()
+  const scope = effectScope()
+  const { websiteName } = useGlobalConfig()
+
+  scope.run(() => {
+    // 国际化切换时更新浏览器标签文本
+    watch(
+      () => locale.value,
+      () => {
+        if (route.meta.title)
+          document.title = `${t(route?.meta?.title as string)} | ${websiteName}`
+      },
+    )
+  })
+
+  onScopeDispose(() => {
+    scope.stop()
+  })
+}
